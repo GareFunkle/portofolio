f6f205dfc71bde75f6e11df17d227178c274baab
GarFunkle <vincent.durret@gmail.com>
2023-01-01 14:44:47 +0100

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,11 +4,35 @@
         <div class="about__wrap-img">
             <img class="about__profil-img" src="../assets/mokup.svg" alt="">
             <div class="test">
-                <p>Bonjour !</p>
+                <img class="about__logo" src="../assets/logo.svg" alt="">
             </div>
-            <div class="about__wrap-profil">
-                <img class="about__img-avatar" src="../assets/avatar.jpg" alt="">
-                <p class="name">Vincent Durret</p>
+            <div class="about__profil">
+                <div class="about__profil-wrap">
+                    <img class="about__img-avatar" src="../assets/avatar.jpg" alt="">
+                    <div class="name__wrap">
+                        <h3 class="name">Vincent Durret</h3>
+                        <p class="name__p">Devellopeur</p>
+                    </div>
+                </div>
+                <hr class="lign">
+                <div class="about__profil-skill">
+                    <div class="skill__wrap">
+                        <img class="skill__img" src="../assets/science.png" alt="">
+                        <span class="skill">React</span>
+                    </div>
+                    <div class="skill__wrap">
+                        <img class="skill__img" src="../assets/vue.svg" alt="">
+                        <span class="skill">Vue</span>
+                    </div>
+                    <div class="skill__wrap">
+                        <img class="skill__img" src="../assets/wordpress.png" alt="">
+                        <span class="skill">Wordpress</span>
+                    </div>
+                    <div class="skill__wrap">
+                        <img class="skill__img" src="../assets/firebase.png" alt="">
+                        <span class="skill">Firebase</span>
+                    </div>
+                </div>
             </div>
         </div>
         <img class="about__img" src="../assets/test.svg" alt="">
@@ -62,7 +86,7 @@ export default {
             z-index: 2;
             left: 27%;
             top: 30vh;
-            width: 30rem;
+            width: 20rem;
         }
 
         .test {
@@ -71,13 +95,14 @@ export default {
             align-items: center;
             justify-content: center;
             z-index: 1;
-            top: 52.5vh;
-            left: 30.7%;
-            width: 23.1rem;
-            height: 50.3rem;
-            border-radius: 41px;
-            animation: start 3s ease-in-out;
+            top: 51.5vh;
+            left: 29.7%;
+            width: 15.5rem;
+            height: 33.7rem;
+            border-radius: 30px;
+            animation: start 7s ease-in-out;
             filter: drop-shadow(1px 1px 40px var(--h1-color));
+            // background: #000;
 
             @keyframes start {
                 0% {
@@ -85,6 +110,10 @@ export default {
                     opacity: 1;
                 }
 
+                50% {
+                    background: black;
+                    opacity: 1;
+                }
 
                 100% {
                     background: black;
@@ -92,32 +121,96 @@ export default {
                 }
             }
 
-            p {
-                color: white;
-                font-weight: bold;
+            .about__logo {
+
+                width: 8rem;
+                visibility: hidden;
+                animation: test 7s ease-in-out;
+
+                @keyframes test {
+                    0% {
+                        visibility: visible;
+                        opacity: 1;
+                    }
+
+                    100% {
+                        visibility: visible;
+                        opacity: 0;
+                    }
+                }
             }
+
         }
 
-        .about__wrap-profil {
-            display: flex;
-            justify-content: center;
-            position: relative;
+        .about__profil {
+            position: absolute;
             z-index: 0;
-            .about__img-avatar {
-                position: relative;
-                width: 5rem;
-                height: 5rem;
-                margin-right: 15px;
-                left: -16.5rem;
-                top: -13rem;
-                border-radius: 50%;
+            top: 56vh;
+            left: 35.8rem;
+            box-sizing: border-box;
+            // border: 1px solid black;
+            height: 30rem;
+            border-radius: 25px;
 
+            .about__profil-wrap {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+
+
+                .about__img-avatar {
+                    width: 5rem;
+                    height: 5rem;
+                    margin-right: 10px;
+                    border-radius: 50%;
+                }
+
+                .name__wrap {
+
+                    .name {
+                        color: var(--h1-color);
+                    }
+
+                    .name__p {
+                        color: var(--p-color);
+                    }
+                }
             }
 
-            .name {
-                position: relative;
-                top: -11.5rem;
-                left: -16rem;
+            .lign {
+                width: 100%;
+                border: 0.01 px solid var(--h1-color);
+                font-weight: bold;
+                margin-top: 10%;
+            }
+
+            .about__profil-skill {
+                display: flex;
+                justify-content: center;
+                flex-direction: column;
+                padding: 1rem;
+                box-sizing: border-box;
+                // border: 1px solid black;
+                height: 25rem;
+
+                .skill__wrap {
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                    margin-top: 0.2rem;
+
+                    .skill__img {
+                        width: 5rem;
+
+
+                    }
+
+
+                    .skill {
+                        // margin-top: 25px;
+                        color: var(--p-color);
+                    }
+                }
             }
 
 
@@ -147,9 +240,11 @@ export default {
 
     .about__left-wrap {
         display: flex;
+        position: relative;
+        top: -15vh;
 
         .about__list {
-            margin-left: 3rem;
+            margin-left: 6rem;
 
             .about__list-point {
                 color: var(--h1-color);
